bfd9c589418561499b51bfc42b2ec602c40cc5d3
alavessi <84871525+alavessi@users.noreply.github.com>
2022-02-26 23:10:50 +0300

    Update H4.5-nearest.cpp

--- hw4/H4.5-nearest.cpp
+++ hw4/H4.5-nearest.cpp
@@ -5,18 +5,18 @@
 
 struct point
 {
-	int id;
+    int id;
     double x, y;
 };
 
 inline bool cmp_x(const point& a, const point& b)
 {
-	return a.x < b.x || (a.x == b.x && a.y < b.y);
+    return a.x < b.x || (a.x == b.x && a.y < b.y);
 }
 
 inline bool cmp_y(const point& a, const point& b)
 {
-	return a.y < b.y;
+    return a.y < b.y;
 }
 
 double mindist;
@@ -24,10 +24,10 @@ int ansa, ansb;
 
 inline void upd_ans(const point& a, const point& b)
 {
-	double dist = sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) + .0);
-	if (std::make_tuple(dist, std::min(a.id, b.id), std::max(a.id, b.id)) <= std::make_tuple(mindist, std::min(ansa, ansb), std::max(ansa, ansb)))
+    double dist = sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) + .0);
+    if (std::make_tuple(dist, std::min(a.id, b.id), std::max(a.id, b.id)) <= std::make_tuple(mindist, std::min(ansa, ansb), std::max(ansa, ansb)))
     {
-		mindist = dist;
+	mindist = dist;
         ansa = std::min(a.id, b.id);
         ansb = std::max(a.id, b.id);
     }
@@ -35,38 +35,38 @@ inline void upd_ans(const point& a, const point& b)
 
 void rec (point* a, int l, int r)
 {
-	if (r - l <= 3)
+    if (r - l <= 3)
     {
-		for (int i = l; i <= r; i++)
+	for (int i = l; i <= r; i++)
         {
-			for (int j = i + 1; j <= r; j++)
+	    for (int j = i + 1; j <= r; j++)
             {
-				upd_ans(a[i], a[j]);
+		upd_ans(a[i], a[j]);
             }
         }
         std::sort(a + l, a + r + 1, &cmp_y);
-		return;
-	}
+	return;
+    }
  
-	int m = (l + r) >> 1;
+    int m = (l + r) >> 1;
     double midx = a[m].x;
-	rec(a, l, m);
+    rec(a, l, m);
     rec(a, m + 1, r);
-	static point t[500000];
-	std::merge(a + l, a + m + 1, a + m + 1, a + r + 1, t, &cmp_y);
-	std::copy(t, t + r - l + 1, a + l);
+    static point t[500000];
+    std::merge(a + l, a + m + 1, a + m + 1, a + r + 1, t, &cmp_y);
+    std::copy(t, t + r - l + 1, a + l);
  
-	int tsz = 0;
-	for (int i = l; i <= r; i++)
+    int tsz = 0;
+    for (int i = l; i <= r; i++)
     {
-		if (abs (a[i].x - midx) < mindist)
+	if (abs (a[i].x - midx) < mindist)
         {
-			for (int j = tsz - 1; j >= 0 && a[i].y - t[j].y < mindist; j--)
+	    for (int j = tsz - 1; j >= 0 && a[i].y - t[j].y < mindist; j--)
             {
-				upd_ans(a[i], t[j]);
+		upd_ans(a[i], t[j]);
             }
             t[tsz++] = a[i];
-		}
+	}
     }
 }
 

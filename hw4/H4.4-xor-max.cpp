70c20ce4687bf633eb3db3179b155dc2c17f94c3
alavessi <84871525+alavessi@users.noreply.github.com>
2022-02-26 23:00:35 +0300

    Update H4.4-xor-max.cpp

--- hw4/H4.4-xor-max.cpp
+++ hw4/H4.4-xor-max.cpp
@@ -4,82 +4,102 @@
 
 void build_xor(int* t, int* a, unsigned v, unsigned tl, unsigned tr)
 {
-	if (tl == tr)
+    if (tl == tr)
     {
-		t[v] = a[tl];
+	t[v] = a[tl];
     }
     else
     {
-		unsigned tm = (tl + tr) >> 1;
-		build_xor(t, a, v << 1, tl, tm);
-		build_xor(t, a, (v << 1) + 1, tm + 1, tr);
-		t[v] = t[v << 1] ^ t[(v << 1) + 1];
-	}
+	unsigned tm = (tl + tr) >> 1;
+	build_xor(t, a, v << 1, tl, tm);
+	build_xor(t, a, (v << 1) + 1, tm + 1, tr);
+	t[v] = t[v << 1] ^ t[(v << 1) + 1];
+    }
 }
 
 void build_max(int* t, int* a, unsigned v, unsigned tl, unsigned tr)
 {
-	if (tl == tr)
+    if (tl == tr)
     {
-		t[v] = a[tl];
+	t[v] = a[tl];
     }
     else
     {
-		unsigned tm = (tl + tr) >> 1;
-		build_max(t, a, v << 1, tl, tm);
-		build_max(t, a, (v << 1) + 1, tm + 1, tr);
-		t[v] = std::max(t[v << 1], t[(v << 1) + 1]);
-	}
+	unsigned tm = (tl + tr) >> 1;
+	build_max(t, a, v << 1, tl, tm);
+	build_max(t, a, (v << 1) + 1, tm + 1, tr);
+	t[v] = std::max(t[v << 1], t[(v << 1) + 1]);
+    }
 }
 
 int Xor(int* t, unsigned v, unsigned tl, unsigned tr, unsigned l, unsigned r)
 {
-	if (l > r)
-		return 0;
-	if (l == tl && r == tr)
-		return t[v];
-	unsigned tm = (tl + tr) >> 1;
-	return Xor(t, v << 1, tl, tm, l, std::min(r, tm)) ^ Xor(t, (v << 1) + 1, tm + 1, tr, std::max(l, tm + 1), r);
+    if (l > r)
+    {
+        return 0;
+    }
+    if (l == tl && r == tr)
+    {
+        return t[v];
+    }
+    unsigned tm = (tl + tr) >> 1;
+    return Xor(t, v << 1, tl, tm, l, std::min(r, tm)) ^ Xor(t, (v << 1) + 1, tm + 1, tr, std::max(l, tm + 1), r);
 }
 
 int max(int* t, unsigned v, unsigned tl, unsigned tr, unsigned l, unsigned r)
 {
-	if (l > r)
-		return INT_MIN;
-	if (l == tl && r == tr)
-		return t[v];
-	unsigned tm = (tl + tr) >> 1;
-	return std::max(max(t, v << 1, tl, tm, l, std::min(r, tm)), max(t, (v << 1) + 1, tm + 1, tr, std::max(l, tm + 1), r));
+    if (l > r)
+    {
+	return INT_MIN;
+    }
+    if (l == tl && r == tr)
+    {
+	return t[v];
+    }
+    unsigned tm = (tl + tr) >> 1;
+    return std::max(max(t, v << 1, tl, tm, l, std::min(r, tm)), max(t, (v << 1) + 1, tm + 1, tr, std::max(l, tm + 1), r));
 }
 
 void update_xor(int* t, unsigned v, unsigned tl, unsigned tr, unsigned pos, int new_val)
 {
-	if (tl == tr)
-		t[v] = new_val;
-	else
+    if (tl == tr)
     {
-		unsigned tm = (tl + tr) >> 1;
-		if (pos <= tm)
-			update_xor(t, v << 1, tl, tm, pos, new_val);
-		else
-			update_xor(t, (v << 1) + 1, tm + 1, tr, pos, new_val);
-		t[v] = t[v << 1] ^ t[(v << 1) + 1];
+        t[v] = new_val;
+    }
+    else
+    {
+	unsigned tm = (tl + tr) >> 1;
+	if (pos <= tm)
+	{
+	    update_xor(t, v << 1, tl, tm, pos, new_val);
 	}
+	else
+	{
+	    update_xor(t, (v << 1) + 1, tm + 1, tr, pos, new_val);
+	}
+	t[v] = t[v << 1] ^ t[(v << 1) + 1];
+    }
 }
 
 void update_max(int* t, unsigned v, unsigned tl, unsigned tr, unsigned pos, int new_val)
 {
-	if (tl == tr)
-		t[v] = new_val;
-	else
+    if (tl == tr)
     {
-		unsigned tm = (tl + tr) >> 1;
-		if (pos <= tm)
-			update_max(t, v << 1, tl, tm, pos, new_val);
-		else
-			update_max(t, (v << 1) + 1, tm + 1, tr, pos, new_val);
-		t[v] = std::max(t[v << 1], t[(v << 1) + 1]);
+	t[v] = new_val;
+    }
+    else
+    {
+	unsigned tm = (tl + tr) >> 1;
+	if (pos <= tm)
+	{
+            update_max(t, v << 1, tl, tm, pos, new_val);
 	}
+	else
+	{
+	    update_max(t, (v << 1) + 1, tm + 1, tr, pos, new_val);
+	}
+	t[v] = std::max(t[v << 1], t[(v << 1) + 1]);
+    }
 }
 
 int main()
